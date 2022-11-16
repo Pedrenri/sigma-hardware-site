8e7c931ab3654d2a6f08f1d8e9f5a2c5046cbdb3
Pedrenri <phenrigoncalves@gmail.com>
2022-11-16 12:01:03 -0300

    hotfix

--- src/components/HomeProduct.js
+++ src/components/HomeProduct.js
@@ -9,29 +9,29 @@ function HomeProduct() {
         <div className="product-holder xl:w-full xl:flex xl:justify-center">
           <ul className="pl-0 md:flex md:items-center ">
             <li className="flex flex-column items-center mb-20">
-              <div className="bg-blue-50 rounded p-6 mx-6 md:mx-3">
+              <div className="bg-blue-50 rounded p-6 mb-4 md:mb-0 mx-6 md:mx-3">
                 <img className="" src={CPU} alt='CPU'/>
               </div>
               <h1>Processadores</h1>
-              <Link to="/products" className="button">
+              <Link to="/products" className="button mt-2">
                 SAIBA MAIS
               </Link>
             </li>
             <li className="flex flex-column items-center mb-20 ">
-              <div className="bg-blue-50 rounded p-6 mx-6 md:mx-3">
+              <div className="bg-blue-50 rounded p-6 mb-4 md:mb-0 mx-6 md:mx-3">
                 <img className="" src={GPU} alt='GPU' />
               </div>{" "}
               <h1>Placas de Vídeo</h1>
-              <Link to="/products" className="button">
+              <Link to="/products" className="button mt-2">
                 SAIBA MAIS
               </Link>
             </li>
             <li className="flex flex-column items-center mb-20 ">
-              <div className="bg-blue-50 rounded p-6 mx-6 md:mx-3">
+              <div className="bg-blue-50 rounded p-6 mb-4 md:mb-0 mx-6 md:mx-3">
                 <img className="" src={MEM} alt='MEM' />
               </div>{" "}
               <h1>Memórias</h1>
-              <Link to="/products" className="button">
+              <Link to="/products" className="button mt-2">
                 SAIBA MAIS
               </Link>
             </li>
