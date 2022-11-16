a38d91feeaae7888679abe2f4e6692a52ddeaf14
Pedrenri <phenrigoncalves@gmail.com>
2022-11-16 11:52:56 -0300

    v1.11

--- src/components/HomeNews.js
+++ src/components/HomeNews.js
@@ -5,43 +5,35 @@ import news2 from "../assets/img/news2.png";
 
 function HomeNews() {
     return (
-        <div className="news-holder flex flex-col lg:flex-row md:gap-x-6 md:px-6">
-          <div className="flex flex-column mb-20 ">
-            <div>
+        <div className="news-holder flex flex-col lg:flex-row md:gap-x-6 px-6 lg:px-20">
+          <div className="flex flex-column mb-20 lg:w-1/2">
+            <div className='h-1/2 overflow-hidden'>
               <img src={news1} />
             </div>
-            <article className="px-6 py-6 text-justify">
-              <h1 className="mb-6">Apollo</h1>
-              <p>
+            <article className="px-3 py-6 text-justify w-full">
+              <h2 className="mb-3">Apollo</h2>
+              <p className='text-md xl:text-xl lg:h-36'>
                 A Sigma Apollo é a GPU definitiva. Ela traz um enorme salto em
                 desempenho, eficiência e gráficos com inteligência artificial.
                 Experimente jogos de alto desempenho, mundos virtuais
                 incrivelmente detalhados, produtividade sem precedentes e novas
-                maneiras de criar. Equipada com a arquitetura Sigma Pantheon,
-                ela vem com 36 GB de memória GDDR6X para oferecer a melhor
-                experiência para jogadores e criadores de conteúdo.
+                maneiras de criar.
               </p>
-              <Link to="/news" className="button">
+              <Link to="/news" className="button font-semibold">
                 VEJA MAIS
               </Link>
             </article>
           </div>
-          <div className="flex flex-column items-center mb-20">
-            <div>
+          <div className="flex flex-column items-center mb-20 lg:w-1/2">
+            <div className='h-1/2 overflow-hidden'>
               <img src={news2} />
             </div>
-            <article className="px-6 py-6 text-justify">
-              <h1 className="mb-6">Pandora</h1>
-              <p>
-                Os Sigma Pandora são a nova linha de processadores Sigma. Eles
-                traz uma vantagem enorme com seus núcleos inteligentes.
-                Experimente jogos de alto desempenho, mundos virtuais
-                incrivelmente detalhados, produtividade sem precedentes e novas
-                maneiras de criar. Equipada com a arquitetura Sigma Pantheon e
-                ainda com 72MB de memória Cache total para oferecer o melhor
-                desempenho para jogadores e criadores em geral.
+            <article className="px-3 py-6 text-justify w-full">
+              <h2 className="mb-3">Pandora</h2>
+              <p className='lg:text-md xl:text-xl lg:h-36'>
+                Os Sigma Pandora são a nova linha de processadores Sigma.
               </p>
-              <Link to="/news" className="button">
+              <Link to="/news" className="button font-semibold">
                 VEJA MAIS
               </Link>
             </article>
